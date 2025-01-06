3c6090abb9b2ee5aeb531dab00452991e0b28779
João Marcos <joaomarcosjoao92@gmail.com>
2025-01-06 13:16:28 -0300

    Adicionado LivroController.

--- api-emakers/src/main/java/com/api/br/api_emakers/controller/LivroController.java
+++ api-emakers/src/main/java/com/api/br/api_emakers/controller/LivroController.java
@@ -1,4 +1,43 @@
 package com.api.br.api_emakers.controller;
 
+import com.api.br.api_emakers.model.dto.request.LivroRequestDTO;
+import com.api.br.api_emakers.model.dto.response.LivroResponseDTO;
+import com.api.br.api_emakers.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Livro")
 public class LivroController {
+    @Autowired
+    private LivroService livroService;
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<LivroResponseDTO>> getAllLivros(){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.getAllLivros());
+    }
+
+    @GetMapping(value = "/{idLivro}")
+    public ResponseEntity<LivroResponseDTO> getLivroById(@PathVariable Integer idLivro){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.getLivroById(idLivro));
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<LivroResponseDTO> createLivro(@RequestBody LivroRequestDTO categoryRequestDto){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.createLivro(categoryRequestDto));
+    }
+
+    @PostMapping(value = "/update/{idLivro}")
+    public ResponseEntity<LivroResponseDTO> updateLivro(@PathVariable Integer idLivro, @RequestBody LivroRequestDTO livroRequestDTO){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.updateLivro(idLivro, livroRequestDTO));
+    }
+
+    @DeleteMapping("/delete/{idLivro}")
+    public ResponseEntity<String> deleteLivro(@PathVariable Integer idLivro){
+        return ResponseEntity.status(HttpStatus.OK).body(livroService.deleteLivro(idLivro));
+    }
 }
